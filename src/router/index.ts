637f63a2d762881ebd526fa06af9194e44e0ea0e
kegra <nukegarara@yahoo.co.jp>
2023-09-26 12:42:57 +0900

    setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ComicListViewVue from "../views/ComicListView.vue";
+import NoteListViewVue from "@/views/NoteListView.vue";
+import ComicDetailsViewVue from "@/views/ComicDetailsView.vue";
+import ComicEpisodeViewVue from "@/views/ComicEpisodeView.vue";
+import NoteEditViewVue from "@/views/NoteEditView.vue";
+import NoteReadViewVue from "@/views/NoteReadView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      component: ComicListViewVue,
+      children: [
+        {
+          path: "comic/:comicid",
+          component: ComicDetailsViewVue,
+          children: [
+            {
+              path: ":episodeid",
+              component: ComicEpisodeViewVue,
+            },
+          ],
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/notes",
+      component: NoteListViewVue,
+      children: [
+        {
+          path: "/edit/:noteid",
+          component: NoteEditViewVue,
+        },
+        {
+          path: "/view/:noteid",
+          component: NoteReadViewVue,
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
