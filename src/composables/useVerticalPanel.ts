342ae4551f3ce3ef632f3fe9a466b9871a2f01b0
kegra <nukegarara@yahoo.co.jp>
2023-09-26 23:38:44 +0900

    useVerticalPanelに分離

---
+++ src/composables/useVerticalPanel.ts
@@ -0,0 +1,104 @@
+import { computed, onMounted, onUnmounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+export const useVerticalPanel = (prevPath: string) => {
+  const router = useRouter();
+
+  const exiting = ref(false);
+  const viewerAnimating = ref(false);
+  const viewerFrameMove = ref(0);
+  const viewerFrameStyle = computed(() => {
+    return {
+      transform: `translateY(${
+        exiting.value ? "100%" : `${viewerFrameMove.value}px`
+      })`,
+      transition: viewerAnimating.value ? "0.3s transform ease" : "none",
+    };
+  });
+  let viewerWheelTimeout = -1;
+  let viewerAnimatingTimeout = -1;
+  const exitWithAnimation = () => {
+    exiting.value = true;
+    viewerAnimating.value = true;
+    viewerAnimatingTimeout = setTimeout(() => {
+      viewerAnimating.value = false;
+      router.push(prevPath);
+    }, 300);
+  };
+  const cancelExiting = () => {
+    viewerFrameMove.value = 0;
+    viewerAnimating.value = true;
+    viewerAnimatingTimeout = setTimeout(() => {
+      viewerAnimating.value = false;
+    }, 300);
+  };
+  const viewerOnWheel = (e: WheelEvent) => {
+    if (e.ctrlKey) return;
+    if (exiting.value) return;
+    viewerFrameMove.value -= e.deltaY;
+    clearTimeout(viewerWheelTimeout);
+    clearTimeout(viewerAnimatingTimeout);
+    if (viewerFrameMove.value > 300) {
+      exiting.value = true;
+      viewerAnimating.value = true;
+      viewerAnimatingTimeout = setTimeout(() => {
+        viewerAnimating.value = false;
+        router.push(prevPath);
+      }, 300);
+      return;
+    }
+    viewerWheelTimeout = setTimeout(cancelExiting, 300);
+  };
+  const viewerOnKeydown = (e: KeyboardEvent) => {
+    console.log("a");
+    if (e.key == "ArrowDown" || e.key == "Escape") {
+      console.log("b");
+      exitWithAnimation();
+    }
+  };
+
+  let touchstartY = 0;
+  let touching1finger = false;
+  const viewerOnTouchChange = (e: TouchEvent) => {
+    if (e.touches.length == 1) {
+      touching1finger = true;
+      touchstartY = e.touches[0].clientY;
+    } else {
+      touching1finger = false;
+      if (!exiting.value) cancelExiting();
+    }
+  };
+  const viewerOnTouchMove = (e: TouchEvent) => {
+    if (touching1finger)
+      viewerFrameMove.value = Math.max(
+        0,
+        e.touches[0].clientY - touchstartY - 50
+      );
+    if (viewerFrameMove.value > 150) {
+      clearTimeout(viewerAnimatingTimeout);
+      exiting.value = true;
+      viewerAnimating.value = true;
+      viewerAnimatingTimeout = setTimeout(() => {
+        viewerAnimating.value = false;
+        router.push(`/comic/${route.params.comicid}`);
+      }, 300);
+      return;
+    }
+  };
+  onMounted(() => {
+    document.addEventListener("wheel", viewerOnWheel);
+    document.addEventListener("keydown", viewerOnKeydown);
+    document.addEventListener("touchstart", viewerOnTouchChange);
+    document.addEventListener("touchmove", viewerOnTouchMove);
+    document.addEventListener("touchend", viewerOnTouchChange);
+  });
+  onUnmounted(() => {
+    document.removeEventListener("wheel", viewerOnWheel);
+    document.removeEventListener("keydown", viewerOnKeydown);
+    document.removeEventListener("touchstart", viewerOnTouchChange);
+    document.removeEventListener("touchmove", viewerOnTouchMove);
+    document.removeEventListener("touchend", viewerOnTouchChange);
+  });
+
+  return { style: viewerFrameStyle };
+};
--- src/views/ComicEpisodeView.vue
+++ src/views/ComicEpisodeView.vue
@@ -1,106 +1,16 @@
 <script setup lang="ts">
 import { computed, onMounted, onUnmounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import { useVerticalPanel } from "@/composables/useVerticalPanel";
 
-const router = useRouter();
 const route = useRoute();
 
-const exiting = ref(false);
-const viewerAnimating = ref(false);
-const viewerFrameMove = ref(0);
-const viewerFrameStyle = computed(() => {
-  return {
-    transform: `translateY(${
-      exiting.value ? "100%" : `${viewerFrameMove.value}px`
-    })`,
-    transition: viewerAnimating.value ? "0.3s transform ease" : "none",
-  };
-});
-let viewerWheelTimeout = -1;
-let viewerAnimatingTimeout = -1;
-const exitWithAnimation = () => {
-  exiting.value = true;
-  viewerAnimating.value = true;
-  viewerAnimatingTimeout = setTimeout(() => {
-    viewerAnimating.value = false;
-    router.push(`/comic/${route.params.comicid}`);
-  }, 300);
-};
-const cancelExiting = () => {
-  viewerFrameMove.value = 0;
-  viewerAnimating.value = true;
-  viewerAnimatingTimeout = setTimeout(() => {
-    viewerAnimating.value = false;
-  }, 300);
-};
-const viewerOnWheel = (e: WheelEvent) => {
-  if (e.ctrlKey) return;
-  if (exiting.value) return;
-  viewerFrameMove.value -= e.deltaY;
-  clearTimeout(viewerWheelTimeout);
-  clearTimeout(viewerAnimatingTimeout);
-  if (viewerFrameMove.value > 300) {
-    exiting.value = true;
-    viewerAnimating.value = true;
-    viewerAnimatingTimeout = setTimeout(() => {
-      viewerAnimating.value = false;
-      router.push(`/comic/${route.params.comicid}`);
-    }, 300);
-    return;
-  }
-  viewerWheelTimeout = setTimeout(cancelExiting, 300);
-};
-const viewerOnKeydown = (e: KeyboardEvent) => {
-  console.log("a");
-  if (e.key == "ArrowDown" || e.key == "Escape") {
-    console.log("b");
-    exitWithAnimation();
-  }
-};
+const verticalPanel = useVerticalPanel(`/comic/${route.params.comicid}`)
 
-let touchstartY = 0;
-let touching1finger = false;
-const viewerOnTouchChange = (e: TouchEvent) => {
-  if (e.touches.length == 1) {
-    touching1finger = true;
-    touchstartY = e.touches[0].clientY;
-  } else {
-    touching1finger = false;
-    if (!exiting.value) cancelExiting();
-  }
-};
-const viewerOnTouchMove = (e: TouchEvent) => {
-  if (touching1finger)
-    viewerFrameMove.value = Math.max(0, e.touches[0].clientY - touchstartY - 50);
-  if (viewerFrameMove.value > 150) {
-    clearTimeout(viewerAnimatingTimeout);
-    exiting.value = true;
-    viewerAnimating.value = true;
-    viewerAnimatingTimeout = setTimeout(() => {
-      viewerAnimating.value = false;
-      router.push(`/comic/${route.params.comicid}`);
-    }, 300);
-    return;
-  }
-};
-onMounted(() => {
-  document.addEventListener("wheel", viewerOnWheel);
-  document.addEventListener("keydown", viewerOnKeydown);
-  document.addEventListener("touchstart", viewerOnTouchChange);
-  document.addEventListener("touchmove", viewerOnTouchMove);
-  document.addEventListener("touchend", viewerOnTouchChange);
-});
-onUnmounted(() => {
-  document.removeEventListener("wheel", viewerOnWheel);
-  document.removeEventListener("keydown", viewerOnKeydown);
-  document.removeEventListener("touchstart", viewerOnTouchChange);
-  document.removeEventListener("touchmove", viewerOnTouchMove);
-  document.removeEventListener("touchend", viewerOnTouchChange);
-});
 </script>
 
 <template>
-  <div :class="$style.viewerFrame" :style="viewerFrameStyle"></div>
+  <div :class="$style.viewerFrame" :style="verticalPanel.style.value"></div>
 </template>
 
 <style module lang="scss">
