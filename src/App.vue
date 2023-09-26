999bc66262709d6b18bbb220bd7574d3bbfb9ab0
kegra <nukegarara@yahoo.co.jp>
2023-09-26 15:40:26 +0900

    ヘッダ

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,33 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <nav>
+  <header :class="$style.header">
+    <nav :class="$style.navbar">
       <RouterLink to="/">漫画</RouterLink> |
       <RouterLink to="/notes">書庫</RouterLink>
     </nav>
+    <div :class="$style.login">
+      <RouterLink to="/login">ログイン / ユーザ登録</RouterLink>
+    </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style module lang="scss">
+.header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+}
+.navbar {
+}
+
+.login {
+  position: absolute;
+  right: 0;
+}
 </style>
