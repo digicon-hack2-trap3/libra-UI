a31931d93d4557bedb4637b2c0db50aa116f8b74
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2023-09-27 18:35:46 +0900

    動作確認中

--- src/views/NoteListView.vue
+++ src/views/NoteListView.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { NoteApi } from "../lib/apis/generated/api";
+import { Configuration } from "../lib/apis/generated/configuration";
+
+const noteApi = new NoteApi(
+  new Configuration({ basePath: "http://localhost:3000" })
+);
+const notes = noteApi.noteGet();
+</script>
 
 <template>
   notelist
   <div>
+    <div v-for="note in notes">
+      {{ note }}
+    </div>
     <RouterLink to="/notes/view/1">note1</RouterLink>
     <RouterLink to="/notes/view/2">note2</RouterLink>
     <RouterLink to="/notes/view/3">note3</RouterLink>
@@ -12,3 +24,4 @@
 </template>
 
 <style module lang="scss"></style>
+
