aa94ee8caaeffbc8a8af0b7f73ae89765c3d5fc1
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2023-09-28 15:51:26 +0900

    NoteReadView, Login実装

--- src/views/LoginRegisterView.vue
+++ src/views/LoginRegisterView.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import {
+  UserApi,
+  type LoginPostRequest,
+} from "../lib/apis/generated/apis/UserApi";
+const username = ref<string>("");
+const password = ref<string>("");
+const userApi = new UserApi();
+const loginresult = ref<string>("");
+const login = () => {
+  const accountdata: LoginPostRequest = {
+    username: username.value,
+    password: password.value,
+  };
+  userApi
+    .loginPost(accountdata)
+    .then(() => (loginresult.value = "ログインに成功しました"));
+};
 </script>
 
 <template>
   <dl>
     <dt>ユーザー名</dt>
-    <dd><input type="text" /></dd>
+    <dd><input type="text" v-model="username" /></dd>
   </dl>
   <dl>
     <dt>パスワード</dt>
-    <dd><input type="password" /></dd>
+    <dd><input type="password" v-model="password" /></dd>
   </dl>
-  <button>ログイン/ユーザー登録</button>
+  <button @click="login">ログイン/ユーザー登録</button>
+  <p>{{ loginresult }}</p>
 </template>
 
-<style module lang="scss">
-</style>
+<style module lang="scss"></style>
+
